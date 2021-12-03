c3f58e15a6cdb3009e3cf3f5b346299a04f1b00b
ProfVillegas <julian.villegas@utrivieramaya.edu.mx>
2021-12-03 14:57:13 -0500

    TI32BIS FRONTEND

---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,25 @@
+<template>
+    <span>
+        <v-app-bar color="deep-purple" dark>
+            <v-app-bar-nav-icon 
+            class="hidden-md-and-up"
+            @click="drawer= !drawer"
+            ></v-app-bar-nav-icon>
+            <v-toolbar-title>Sys.Dev</v-toolbar-title>
+            <v-spacer class="hidden-md-and-up"></v-spacer>
+            <v-btn to="/" flat class="hidden-md-and-up">Home</v-btn>
+            <v-spacer class="hidden-md-and-up"></v-spacer>
+            <v-btn to="/about" flat class="hidden-md-and-up">About</v-btn>
+        </v-app-bar>
+    </span>
+</template>
+<script>
+export default {
+    name:"AppNavigation",
+    data(){
+        return{
+            drawer:false
+        }
+    }
+}
+</script>
